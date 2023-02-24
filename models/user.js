4c95c6ea97c20e94a123b75b1ddc7db919a3bbdb
Tozzi <nicholastozzi@gmail.com>
2023-02-23 21:13:31 -0500

    added file structure changes in views folder, and started template for user.js

---
+++ models/user.js
@@ -0,0 +1,73 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+      },
+      top5: {
+
+      },
+    watched: {  
+     //TODO finish params 
+    },
+    watchlist: {
+        //TODO finish params 
+    },
+    followingActors: {
+//TODO finish params 
+    },
+    followingUsers: {
+//TODO finish params 
+    },
+    genre {
+//TODO finish params 
+type: array [
+    
+]
+    },
+
+    },
+  },
+  {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+);
+
+module.exports = User;
