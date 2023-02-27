b2f6abd4a05f29891cdd4ca79040f085f94dd6d8
Paul Dangelo <pdangelo1988@gmail.com>
2023-02-27 15:47:13 -0500

    modified .gitignore

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,4 +2,35 @@ const router = require("express").Router();
 const userRoutes = require("./userRoutes");
 const profileRoutes = require("./profileRoutes");
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 module.exports = router;
