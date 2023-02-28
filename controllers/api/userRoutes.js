1bb48b1c518d13899cdc22999b19f1b245882e5c
Paul Dangelo <pdangelo1988@gmail.com>
2023-02-28 15:27:32 -0500

    Started login route. Will finish tonight in class

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,54 @@
+// To do
+// Movie Model
+const router = require("express").Router();
+const { User } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+//For the user to sign up. If we would like to put a username in the model we can.
+router.post('/signUp', async (req, res) => {
+    try {
+        const userData = await User.create({
+            // username: req.body.username,
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        })
+        req.session.save(() => {
+            req.session.id = userData.id
+            req.session.logged_in = true
+
+            //200 code is for testing purposes
+            res.status(200).json({message:"Successish!"});
+            // res.redirect("/profile")
+        })
+    }
+    catch(err) {
+        res.status(500).json(err);
+        //Collaborate with front end.   
+    } 
+});
+
+router.post('/login', withAuth, async (req, res) => {
+    try {
+        const userData = await userData.fineOne( {where: {email: req.body.email } })
+
+        if (!userData) {
+            res.status(400).json({ message: "Incorrect E-Mail or Password. Check your spelling and capitalization and try again"})
+        return
+        }
+
+        const correctPass = await userData.checkPassword(req.body.password)
+
+        if (!correctPass) {
+            res.status(400).json(message,"You are now logged in") //comma? Thought it was supposed to be a :
+        }
+
+    }
+    catch {} //not done yet
+});
+
+// router.delete('/:id', withAuth, async (req, res) => {
+
+// });
+
+module.exports = router;
