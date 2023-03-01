73843409b3129d2ec1db80a22c46cc3387c35679
Jose Del Valle <jdelvalle88@live.com>
2023-02-28 20:38:04 -0500

    CRUD added to profileRoutes.js file

--- utils/helpers.js
+++ utils/helpers.js
@@ -5,3 +5,12 @@ module.exports = {
     },
   };
   
+  const result = add(new Date(2014, 8, 1, 10, 19, 50), {
+    years: 2,
+    months: 9,
+    weeks: 1,
+    days: 7,
+    hours: 5,
+    minutes: 9,
+    seconds: 30,
+  })
