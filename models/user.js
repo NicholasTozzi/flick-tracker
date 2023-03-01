d5d3ef8bfcc51e76097bf6d2dba66139584dd77d
Tozzi <nicholastozzi@gmail.com>
2023-02-28 22:05:28 -0500

    fixed seeds. Wahoo

--- models/user.js
+++ models/user.js
@@ -14,23 +14,20 @@ User.init(
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
-      autoIncrement: true
+      autoIncrement: true,
     },
     username: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
-      validate: {
-        isEmail: true
-      },
     },
     password: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        len: [8]
-      }
-    }
+        len: [8],
+      },
+    },
   },
   {
     hooks: {
