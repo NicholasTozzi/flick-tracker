55141cd2060afde9244595f1105491598b3dc0ab
Jose Del Valle <jdelvalle88@live.com>
2023-03-03 18:04:59 -0500

    date-fns added for future, tomorrow, and to format

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,16 +1,37 @@
-module.exports = {
-    format_date: (date) => {
-      // Format date as MM/DD/YYYY
-      return date.toLocaleDateString();
-    },
-  };
+import { format, parseJSON, addDays, isFuture  } from 'date-fns'
+
+//Now
+import format from 'date-fns/format'
+
+const flickTrackerApp = () => {
+
+  const date = format(new Date(), 'MM/dd/yyyy');
+  const tomorrow =  addDays (date, 1);
+  const inTheFuture = isFuture(new Date());
+  const parse = parseJSON(date);
+}
+
+export default flickTrackerApp;
+
+
+// module.exports = {
+//   format_date: (date) => {
+    
+//     // Format date as MM/DD/YYYY
+//       const date = new Date();
+//       return date.toLocaleDateString();
+//     },
+//     future_date: (futureDate) => {
+//       //future date from now
+//       const futureDate = isFuture(new Date(3, 2, 2023), 30);    
+//       return futureDate.toLocaleDateString();
+//     },
+//     set_date: (setDate) => {
+//       //Set the day of the month to the given date.
+//       const date = setDate(new Date(2023, 3, 2), 30);
+//       return setDate.toLocaleDateString();
+//     }
+//   };
   
-  const result = add(new Date(2014, 8, 1, 10, 19, 50), {
-    years: 2,
-    months: 9,
-    weeks: 1,
-    days: 7,
-    hours: 5,
-    minutes: 9,
-    seconds: 30,
-  })
+
+  
