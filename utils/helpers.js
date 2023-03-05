c6705031762e3c7f5d10d0b736d8e1aeba61558c
Jose Del Valle <jdelvalle88@live.com>
2023-03-05 17:37:53 -0500

    small edits to dates on helper file

--- utils/helpers.js
+++ utils/helpers.js
@@ -8,15 +8,15 @@ module.exports = {
         //today's date
         return currentDate.toLocaleDateString();
     },
-  tomorrow_date: (tomorrowDate) => {
+  addDay: (tomorrowDate) => {
     //adding number of days from today
       return tomorrowDate.toLocaleDateString(currentDate, 1);
     },
-  future_date: (futureDate) => {
+  isFuture: (futureDate) => {
     //future date from now
       return futureDate.toLocaleDateString(new Date(05, 04, 2023));
     },
-  set_date: (setDate) => {
+  setDate: (setDate) => {
     //Set the day of the month to the given date.
       return setDate.toLocaleDateString(new Date(03, 06, 2023), {month: 12, date: 25, year: 2023});
     },
