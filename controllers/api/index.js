7107ce877e23fd165f98e5010942204a6282bedd
Paul Dangelo <pdangelo1988@gmail.com>
2023-03-06 13:38:44 -0500

    Added review routes and fixed some bootstrap issues with if each statememnts

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
 const userRoutes = require("./userRoutes");
 const profileRoutes = require("./profileRoutes");
+const watchRoutes = require("./watchRoutes");
 
 router.use("/user", userRoutes)
 
 router.use("/profile", profileRoutes)
 
-// router.use("/watch", watchRoutes) // for watch.js and the routes file
+router.use("/watch", watchRoutes) // for watch.js and the routes file
 
 module.exports = router;
--- controllers/api/watchRoutes.js
+++ controllers/api/watchRoutes.js
@@ -1,15 +1,25 @@
-// const router = require("express").Router();
-// const { User, Profile } = require('../../models');
-// const withAuth = require("../utils/auth");
+const router = require("express").Router();
+const { Profile } = require('../../models');
 
-// router.post('/', async (req, res) => {
-    
-// })
+router.post('/', async (req, res) => {
+    try {
+        const watched = await Profile.create(req.body)
+        res.status(200).json(watched)
+    } catch (err) { 
+      res.status(400).json(err)
+    }
+})
 
-// router.post('/later', async (req, res) => {
+router.post('/later', async (req, res) => {
+    try {
+        const watchLater = await Profile.create(req.body)
+        res.status(200).json(watchLater)
+    } catch (err) { 
+      res.status(400).json(err)
+    }
+})
 
-// })
-
+module.exports = router
 
 // routes for buttons 
 // one for "watched" will be /
