9cea75dda8f9e7a11b7768807e4f1336cdc1fca8
AveryNewhart <newhartreeceavery@gmail.com>
2023-03-05 21:02:09 -0500

    already

--- controllers/api/watchRoutes.js
+++ controllers/api/watchRoutes.js
@@ -1,3 +1,7 @@
 // routes for buttons 
 // one for "watched" will be /
 // one for "watch later" will be /watch/later
+
+// router.post('/', async (req, res) => {
+
+//  router.post('/later', async (req, res) => {
