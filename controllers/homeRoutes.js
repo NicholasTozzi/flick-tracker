7aa7d6736833c6db302c3049fbdfa9f8b2d9cfb6
AveryNewhart <newhartreeceavery@gmail.com>
2023-03-06 14:11:43 -0500

    withAuth for community in home routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -56,7 +56,7 @@ router.get("/", async (req, res) => {
 // });
 
 // Use withAuth middleware to prevent access to route
-router.get("/community", async (req, res) => {
+router.get("/community", withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
