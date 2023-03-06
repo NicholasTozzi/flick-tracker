0e5fb9739097637ea63346e2f759119e9992e166
Paul Dangelo <pdangelo1988@gmail.com>
2023-03-06 18:22:49 -0500

    sending staged changes

--- controllers/api/reviewPost.js
+++ controllers/api/reviewPost.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-// route to post review
-    const router = require('express').Router();
-    const { Review } = require('../../models'); //getting Review from review model
-    const userAuth = require('../../utils/auth');
-
-    router.post('/', userAuth, async (req, res) => {
-        try {
-          const newBlog = await Review.create({
-            blog_title: req.body.blogTitle,
-            blog_content: req.body.blogContent,
-            blog_creator: req.session.user_id,
-          });
-      
-          res.status(200).json(newBlog);
-        } catch (err) {
-          res.status(400).json(err);
-          console.log(err)
-        }
-      });
-    
-      module.exports = router;
-=======
 //route to post review
 const router = require("express").Router();
 const { Review } = require("../../models"); //getting Review from review model
@@ -77,4 +54,3 @@ router.post("/", userAuth, async (req, res) => {
 
 module.exports = router;
 module.exports = axios;
->>>>>>> 281fba4b51d4de6b0f15cc7085d203e7b8fc5669
