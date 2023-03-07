8afcea4b1dc8bf8b80748dbb1e468eaa389324c7
Tozzi <nicholastozzi@gmail.com>
2023-03-07 15:11:34 -0500

    api

--- controllers/api/reviewPost.js
+++ controllers/api/reviewPost.js
@@ -3,41 +3,30 @@ const router = require("express").Router();
 const { Review } = require("../../models"); //getting Review from review model
 const userAuth = require("../../utils/auth");
 const axios = require("axios");
-const axiosInstance = axios.create({});
 
-// axiosInstance.post("/movie", async (req, res) => {
-//    const options = {
-//      method: "POST",
-//      url: "https://imdb8.p.rapidapi.com/auto-complete",
-//      params: { q: "cars" },
-//      headers: {
-//        "X-RapidAPI-Key": "d65415b0b4msha2b92f5899f8885p1dbaa1jsne278af2bc260",
-//        "X-RapidAPI-Host": "imdb8.p.rapidapi.com",
-//      },
-//    };
+router.post("/movie", (req, res) => {
+  const options = {
+    method: "GET",
+    url: "https://imdb8.p.rapidapi.com/auto-complete",
+    params: { q: req.body.movieTitle },
+    headers: {
+      "X-RapidAPI-Key": "d65415b0b4msha2b92f5899f8885p1dbaa1jsne278af2bc260",
+      "X-RapidAPI-Host": "imdb8.p.rapidapi.com",
+    },
+  };
 
-//    await axiosInstance
-//      .request(options)
-//    .then(function(response) {
-//        console.log(response.data);
-//      })
-//      .catch(function (error) {
-//        console.error(error);
-//      });
-// // console.log("isran")
-//  });
+  axios
+    .request(options)
+    .then(function (response) {
+      console.log(response.data.d);
+      res.json(response.data.d);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+});
 
-// axios.post('/search', ( req, res ) {
-//   axios.post('/search', {
-//     movie: movieTitle,
-//   })
-//   .then(function (response) {
-//     console.log(response);
-//   })
-//   .catch(function (error) {
-//     console.log(error);
-//   });
-// })
+// axios.post("/search", (req, res) => {});
 
 router.post("/", userAuth, async (req, res) => {
   try {
@@ -55,4 +44,3 @@ router.post("/", userAuth, async (req, res) => {
 });
 
 module.exports = router;
-module.exports = axios;
--- public/javascript/review.js
+++ public/javascript/review.js
@@ -1,26 +1,26 @@
-const newReviewSubmit = async event => {
+const newReviewSubmit = async (event) => {
   event.preventDefault();
 
   const reviewTitle = document.querySelector("#review-title").value.trim();
   const reviewContent = document.querySelector("#review-content").value.trim();
 
-   if (reviewTitle && reviewContent) {
-     const response = await fetch(`/api/review`, {
-       method: "POST",
-       body: JSON.stringify({ reviewTitle, reviewContent }),
-       headers: {
-         "Content-Type": "application/json"
-       }
-     });
+  if (reviewTitle && reviewContent) {
+    const response = await fetch(`/api/review`, {
+      method: "POST",
+      body: JSON.stringify({ reviewTitle, reviewContent }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
-     if (response.ok) {
-       document.location.replace("/profile");
-       alert("Review Created");
-     } else {
-       alert("Failed to create review");
-     }
-   }
- };
+    if (response.ok) {
+      document.location.replace("/profile");
+      alert("Review Created");
+    } else {
+      alert("Failed to create review");
+    }
+  }
+};
 
 //   const postMovie = async event => {
 //      event.preventDefault();
@@ -44,34 +44,31 @@ const newReviewSubmit = async event => {
 //          }
 //      };
 
-    const search = async event => {
-        event.preventDefault();
-    
-        const movieTitle = document.querySelector("#movie-title").value.trim();
-    
-        if (movieTitle) {
-            const response = await fetch(`/api/review/movie`, {
-                method: "GET",
-                body: JSON.stringify({movieTitle}),
-                headers: {
-                    "X-RapidAPI-Key": "d65415b0b4msha2b92f5899f8885p1dbaa1jsne278af2bc260",
-                    "X-RapidAPI-Host": "imdb8.p.rapidapi.com",
-                    "Content-Type": "application/json",
-                  },
-            });
-    
-            if (response.ok) {
-                document.location.replace("/profile");
-                alert("Movie Searched");
-              } else {
-                alert("Failed to search movie!");
-              }
-            }
-        };
+const search = async (event) => {
+  event.preventDefault();
+  const movieTitle = document.querySelector("#movie-title").value.trim();
+
+  if (movieTitle) {
+    const response = await fetch(`/api/review/movie`, {
+      method: "POST",
+      body: JSON.stringify({ movieTitle }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+    if (response.ok) {
+      // document.location.replace("/profile");
+      alert("Movie Searched");
+    } else {
+      alert("Failed to search movie!");
+    }
+  }
+};
 
 // const search = async event => {
 //     event.preventDefault();
-    
 
 //     const options = {
 //         method: 'GET',
@@ -92,13 +89,9 @@ const newReviewSubmit = async event => {
 //         };
 
 document
-.querySelector(".new-review")
-.addEventListener("submit", newReviewSubmit);
+  .querySelector(".new-review")
+  .addEventListener("submit", newReviewSubmit);
 
-document
-.querySelector("new-movie")
-.addEventListener("submit", search);
- routes=======
+document.querySelector("#new-movie").addEventListener("click", search);
 // document
 // .querySelector("search-movie")// .addEventListener("submit", postMovie);
- main
